c84f531dd9c61c6722ffc6a98d81f86aaa955c4c
Daniel <121930995+danielcodes-xyz@users.noreply.github.com>
2024-02-05 20:15:34 -0800

    First Commit - Testing Desktop App
    Just learning with the Desktop App.

---
+++ script.js
@@ -0,0 +1,41 @@
+const itemForm = document.getElementById('item-form');
+const itemInput = document.getElementById('item-input');
+const itemList = document.getElementById('item-list');
+
+function addItem(e) {
+    e.preventDefault();
+
+    const newItem = itemInput.value;
+    // Validate Input 
+    if (newItem === '') {
+        alert('Please add an item')
+    }
+
+    // Create list item
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(newItem));
+
+    const button = newButton('remove-item btn-link text-red');
+    li.appendChild(button);
+
+    itemList.appendChild(li);
+
+    itemInput.value = '';
+
+}
+
+function newButton (classes) {
+    const button = document.createElement('button');
+    button.className = classes;
+    const icon = newIcon('fa-solid fa-xmark');
+    button.appendChild(icon);
+    return button;
+}
+function newIcon (classes) {
+    const icon = document.createElement('i');
+    icon.className = classes;
+    return icon;
+}
+
+// Event Listeners
+itemForm.addEventListener('submit', addItem);
