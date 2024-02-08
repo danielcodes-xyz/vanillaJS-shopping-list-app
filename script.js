3a181b745568fca2731e5dad4facdccb789de831
Daniel <121930995+danielcodes-xyz@users.noreply.github.com>
2024-02-07 20:55:40 -0800

    Added some functionality
    Added functions to prevent adding the same item twice.
    Added Edit Mode for editing items.
    Added Function for switching colors and changing the ADD ITEM button to EDIT ITEM.

--- script.js
+++ script.js
@@ -4,17 +4,8 @@ const itemList = document.getElementById('item-list');
 const clearButton = document.getElementById('clear');
 const listFilter = document.getElementById('filter');
 const itemArray = document.querySelectorAll('li');
-
-const itemMemory = [];
-
-hideOrShow();
-function hideOrShow() {
-if (itemList.childElementCount === 0) {
-    hideList();
-}else {
-    showList();
-}
-}
+let isEditMode = false;
+const editButton = document.getElementById('edit-button');
 
 function displayItems() {
     const itemsFromStorage = getItemsFromStorage();
@@ -46,6 +37,18 @@ function onAddItemSubmit(e) {
     // Validate Input 
     if (newItem === '') {
         alert('Please add an item');
+        return;
+    }
+
+    if (isEditMode) {
+        const itemToEdit = document.querySelector('.edit-mode');
+
+        removeItemFromStorage(itemToEdit.textContent);
+        itemToEdit.classList.remove('edit-mode');
+        itemToEdit.remove();
+        isEditMode = false;
+    }else if (checkIfItemExists(newItem)) {
+        alert('This item is already on your list.');
         return;
     }
     // Create item DOM element
@@ -107,39 +110,96 @@ function getItemsFromStorage() {
     return itemsFromStorage;
 }
 
-function removeItem (e) {
-    console.log('clicked');
+function onItemClick (e) {
     if (e.target.parentElement.classList.contains('remove-item')) {
-        if (confirm('Are you sure?')) {
-        e.target.parentElement.parentElement.remove();
-        hideOrShow();
-        }    
+        removeItem(e.target.parentElement.parentElement);
+    }else {
+        if (e.target !== itemList) {
+        setItemToEdit(e.target);
+    }}
 }
+
+function checkIfItemExists(item) {
+    const itemsFromStorage = getItemsFromStorage();
+    return itemsFromStorage.includes(item);
+}
+
+function setItemToEdit(item) {
+    isEditMode = true;
+
+    itemList
+        .querySelectorAll('li')
+        .forEach((i) => i.classList.remove('edit-mode'));
+
+    item.classList.add('edit-mode');
+    itemInput.value = item.textContent;
+    editButton.classList.add('edit-button');
+    editButton.innerHTML = '<i class="fa-solid fa-pen"></i> Edit Item'
+}
+
+function removeItem(item) {
+    if (confirm('Are you sure?')) {
+    item.remove();
+    }
+    removeItemFromStorage(item.textContent);
+
+    hideOrShow();
+}
+
+function removeItemFromStorage(item) {
+
+    let itemsFromStorage = getItemsFromStorage();
+
+    itemsFromStorage = itemsFromStorage.filter((i) => i !== item);
+
+    localStorage.setItem('items', JSON.stringify(itemsFromStorage));
 }
 
 function clearItems() {
     if (confirm('Are you sure?')) {
     while (itemList.firstChild) {
-        itemList.removeChild(itemList.firstChild);
-    }}
+            itemList.removeChild(itemList.firstChild);
+        }
+    }
+
+    // Clear from localStorage
+    localStorage.removeItem('items');
+
     hideOrShow();
 }
 
+hideOrShow();
+function hideOrShow() {
+if (itemList.childElementCount === 0) {
+    hideList();
+}else {
+    showList();
+}
+}
 // When list is empty, hide filter and clear button.
 function hideList() {
     clearButton.style.display = 'none';
     listFilter.style.display = 'none';
+    editButton.classList = 'btn';
+    editButton.innerHTML = '<i class="fa-solid fa-plus"></i> Add Item';
+    itemInput.value = '';
+    isEditMode = false;
+
 }
 function showList() {
     clearButton.style.display = 'block';
     listFilter.style.display = 'block';
+    editButton.classList = 'btn';
+    editButton.innerHTML = '<i class="fa-solid fa-plus"></i> Add Item';
+    itemInput.value = '';
+    isEditMode = false;
 }
 
 
 function init() {
 // Event Listeners
 itemForm.addEventListener('submit', onAddItemSubmit);
-itemList.addEventListener('click', removeItem);
+itemList.addEventListener('click', onItemClick);
 clearButton.addEventListener('click', clearItems);
 listFilter.addEventListener('input', filterItems);
 document.addEventListener('DOMContentLoaded', displayItems);
